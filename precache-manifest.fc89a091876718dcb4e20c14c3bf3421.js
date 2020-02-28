1126c0b0af438f01f353b0de44374cc2879ab8ce
Aivaras <lukauskasaivaras@gmail.com>
2020-02-28 09:38:24 +0200

    Updates

---
+++ precache-manifest.fc89a091876718dcb4e20c14c3bf3421.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcbc93ac8f82abf64e6f94eb48d012e3",
+    "url": "/notes-app/index.html"
+  },
+  {
+    "revision": "ec63cb0d3e055dd003db",
+    "url": "/notes-app/static/css/main.2a6d66b3.chunk.css"
+  },
+  {
+    "revision": "2473a6f7f0d5c8e787d0",
+    "url": "/notes-app/static/js/2.c73adfce.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/notes-app/static/js/2.c73adfce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec63cb0d3e055dd003db",
+    "url": "/notes-app/static/js/main.10fdd761.chunk.js"
+  },
+  {
+    "revision": "2130a92484021e424dbc",
+    "url": "/notes-app/static/js/runtime-main.88f094ce.js"
+  }
+]);
